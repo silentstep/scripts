4855a00c59a91692a48c57ab50f4577ef128e61c
Ventsi Nurkov <ventsislav.nurkov@gmail.com>
2017-11-22 11:11:18 +0100

    added provision script

---
+++ provision.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+./initialize.sh
+./db-creation.sh
+./fetch-hook.sh
---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: test.sh
+# 
+#         USAGE: ./test.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2017-11-22 11:07:54
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
+echo 'Provisioning done!'
